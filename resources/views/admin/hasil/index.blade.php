2b00f345dc30ad01350300ac18fba26b95c8ce6a
asrani <asrani.27@gmail.com>
2025-01-10 16:43:50 +0800

    f

--- resources/views/admin/hasil/index.blade.php
+++ resources/views/admin/hasil/index.blade.php
@@ -1,58 +1,61 @@
 @extends('layouts.app')
 @push('css')
-    
+
 @endpush
 @section('content')
 
 <div class="row column_title">
-  <div class="col-md-12">
-     <div class="page_title">
-        <h2>Data Hasil</h2>
-     </div>
-  </div>
+   <div class="col-md-12">
+      <div class="page_title">
+         <h2>Data Hasil</h2>
+      </div>
+   </div>
 </div>
 <div class="white_shd full margin_bottom_30">
-  <div class="full graph_head">
-     <div class="heading1 margin_0">
-       
-      <a href="/superadmin/hasil/create" class="btn btn-flat btn-sm btn-primary"><i class="fa fa-plus"></i> Tambah Data</a>
-     </div>
-  </div>
-  <div class="table_section padding_infor_info">
-     <div class="table-responsive-sm">
-        <table class="table table-bordered">
-           <thead>
-              <tr style="background-color: rgb(52, 52, 51); font-weight:bold;color:aliceblue">
-                <th>No</th>
-                <th>Tanggal</th>
-                <th>Rapat Komisi</th>
-                <th>Rapat Pansus</th>
-                <th>Notulensi</th>
-                <th>Catatan</th>
-                <th>Aksi</th>
-              </tr>
-           </thead>
-           <tbody>
-            
-            @foreach ($data as $key => $item)
-            <tr>
-              <td>{{$data->firstItem() + $key}}</td>
-              <td>{{\Carbon\Carbon::parse($item->created_at)->translatedFormat('d F Y')}}</td>
-              <td>{{$item->rapatkomisi == null ? null : $item->rapatkomisi->nama}}</td>
-              <td>{{$item->rapatpansus == null ? null : $item->rapatpansus->nama}}</td>
-              <td>{{$item->notulensi}}</td>
-              <td>{{$item->catatan}}</td>
-              <td>
-                <a href="/superadmin/hasil/edit/{{$item->id}}" class="btn btn-flat btn-sm btn-success"><i class="fa fa-edit"></i> Edit</a>
-                <a href="/superadmin/hasil/delete/{{$item->id}}" class="btn btn-flat btn-sm btn-danger" onclick="return confirm('Yakin ingin dihapus?');"><i class="fa fa-trash"></i> Delete</a>
-              </td>
-            </tr>
-            @endforeach
-           </tbody>
-        </table>
-     </div>
-  </div>
-  {{$data->links()}}
+   <div class="full graph_head">
+      <div class="heading1 margin_0">
+
+         <a href="/superadmin/hasil/create" class="btn btn-flat btn-sm btn-primary"><i class="fa fa-plus"></i> Tambah
+            Data</a>
+      </div>
+   </div>
+   <div class="table_section padding_infor_info">
+      <div class="table-responsive-sm">
+         <table class="table table-bordered">
+            <thead>
+               <tr style="background-color: rgb(52, 52, 51); font-weight:bold;color:aliceblue">
+                  <th>No</th>
+                  <th>Tanggal</th>
+                  <th>Rapat Komisi</th>
+                  <th>Rapat Pansus</th>
+                  <th>Notulensi</th>
+                  <th>Catatan</th>
+                  <th>Aksi</th>
+               </tr>
+            </thead>
+            <tbody>
+
+               @foreach ($data as $key => $item)
+               <tr>
+                  <td>{{$data->firstItem() + $key}}</td>
+                  <td>{{\Carbon\Carbon::parse($item->created_at)->translatedFormat('d F Y')}}</td>
+                  <td>{{$item->rapatkomisi == null ? null : $item->rapatkomisi->nama}}</td>
+                  <td>{{$item->rapatpansus == null ? null : $item->rapatpansus->nama}}</td>
+                  <td>{{$item->notulensi}}</td>
+                  <td>{{$item->catatan}}</td>
+                  <td>
+                     <a href="/superadmin/hasil/edit/{{$item->id}}" class="btn btn-flat btn-sm btn-success"><i
+                           class="fa fa-edit"></i> Edit</a>
+                     <a href="/superadmin/hasil/delete/{{$item->id}}" class="btn btn-flat btn-sm btn-danger"
+                        onclick="return confirm('Yakin ingin dihapus?');"><i class="fa fa-trash"></i> Delete</a>
+                  </td>
+               </tr>
+               @endforeach
+            </tbody>
+         </table>
+         {{$data->links()}}
+      </div>
+   </div>
 </div>
 
 @endsection
